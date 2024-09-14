33908c1e13377b80a4c8ac9d5aa0329a84568667
Nathanael Rachmat <19623100@std.stei.itb.ac.id>
2024-09-15 01:50:53 +0700

    feat: webcam

--- halopips/app/(protected)/settings/profile/ProfileCard.tsx
+++ halopips/app/(protected)/settings/profile/ProfileCard.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import { updateProfile } from "@/action/user";
@@ -6,22 +6,23 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
-const ProfileCard = ({ initialFullname,
-    initialNickname, 
-    initialFakultas,
-    initialNim,
-    initialInstagram,
-    initialLinkedin,
-    initialIdline,
-}: { 
-    initialFullname: string,
-    initialNickname: string,
-    initialFakultas: string,
-    initialNim: string,
-    initialInstagram: string,
-    initialLinkedin: string,
-    initialIdline: string,
- }) => {
+const ProfileCard = ({
+  initialFullname,
+  initialNickname,
+  initialFakultas,
+  initialNim,
+  initialInstagram,
+  initialLinkedin,
+  initialIdline,
+}: {
+  initialFullname: string;
+  initialNickname: string;
+  initialFakultas: string;
+  initialNim: string;
+  initialInstagram: string;
+  initialLinkedin: string;
+  initialIdline: string;
+}) => {
   const [message, setMessage] = useState("");
   const [isError, setIsError] = useState(false);
 
@@ -41,8 +42,8 @@ const ProfileCard = ({ initialFullname,
   };
 
   return (
-    <div className="p-2">
-      <form action={handleUpdateProfile}>
+    <div className="p-2 h-full">
+      <form className="h-full" action={handleUpdateProfile}>
         <Label className="font-bold text-xl font-mono" htmlFor="fullName">
           Full Name
         </Label>
@@ -127,10 +128,12 @@ const ProfileCard = ({ initialFullname,
           className="rounded-xl bg-white h-12 text-lg text-pips-500 font-semibold w-96"
           required
         />
-        <Button type="submit" className="mt-4">Update Profile</Button>
+        <Button type="submit" className="mt-4">
+          Update Profile
+        </Button>
       </form>
       {message && (
-        <p className={`mt-4 ${isError ? 'text-red-500' : 'text-green-500'}`}>
+        <p className={`mt-4 ${isError ? "text-red-500" : "text-green-500"}`}>
           {message}
         </p>
       )}
