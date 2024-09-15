cb770d912528b3b96bf99ef8b4f6840d82d1086b
Muhammad Raihaan Perdana <perdanaraihan96@gmail.com>
2024-09-15 11:03:57 +0700

    remove login google button

--- halopips/components/auth/FormLogin.tsx
+++ halopips/components/auth/FormLogin.tsx
@@ -3,8 +3,7 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { login, signInGoogle } from "@/action/user";
-import { IconBrandGoogle } from "@tabler/icons-react";
+import { login } from "@/action/user";
 import { Button } from "../ui/button";
 
 const FormLogin = () => {
